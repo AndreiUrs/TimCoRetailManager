1bbfb379e49786c889639ded6f1ffe1378c8abb2
AndreiUrs <ursachi.andrei1991@gmail.com>
2020-05-12 19:33:39 +0200

    Design and model Salev View

---
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,115 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set { _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+
+        private string _itemQuantity;
+
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+        public string SubTotal
+        {
+            //replace with calculations
+            get => "$0.00";
+        }
+
+
+        public string Tax
+        {
+            //replace with calculations
+            get => "$0.00";
+        }
+
+        public string Total
+        {
+            //replace with calculations
+            get => "$0.00";
+        }
+
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+                //Make sure something is selected
+                //Make sure there is an item quantity
+                return output;
+            }
+        }
+        public void AddToCart()
+        {
+
+        }
+
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+                //Make sure something is selected
+                return output;
+            }
+        }
+
+        public void RemoveFromCart()
+        {
+
+        }
+
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+                //Make sure something in the cart
+                return output;
+            }
+        }
+
+        public void CheckOut()
+        {
+
+        }
+    }
+}
